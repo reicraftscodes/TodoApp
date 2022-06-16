527bd63ef1a9b2a845e03084b79878ddca7e44a3
May <sanejolm@cardiff.ac.uk>
2022-06-16 01:56:22 +0100

    Fix typo

--- todo-list-spring/src/main/resources/db/migration/afterMigrate.sql
+++ todo-list-spring/src/main/resources/db/migration/afterMigrate.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE TODO;
 
-INSERT INTO TODO (id, text, done) values (1, 'Learn react redux architecture', 'true');
-INSERT INTO TODO (id, text, done) values (2, 'Practice advance react state, actions, and reducers', 'true');
+INSERT INTO TODO (id, text, done) values (1, 'Learn React Redux Architecture', 'true');
+INSERT INTO TODO (id, text, done) values (2, 'Code Unit Test', 'true');
 INSERT INTO TODO (id, text, done) values (3, 'Refactor Unit Tests', 'false');
 INSERT INTO TODO (id, text, done) values (4, 'Coding Integration Tests', 'false');
 INSERT INTO TODO (id, text, done) values (5, 'Refactor Integration Tests', 'true');
